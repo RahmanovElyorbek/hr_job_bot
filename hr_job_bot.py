46cda230c642af4422d1b6fcad2fc0342de0404d
Elyorbek Xalmuxammatovich Rahmanov <132456769+RahmanovElyorbek@users.noreply.github.com>
2026-02-07 20:54:14 +0500

    webhook

--- hr_job_bot.py
+++ hr_job_bot.py
@@ -10,7 +10,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
+from aiogram.utils.executor import start_webhook
 
 # ================= LOGGING =================
 logging.basicConfig(level=logging.INFO)
@@ -176,4 +176,29 @@ async def global_error_handler(update, exception):
 
 # ================= RUN =================
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+
+WEBHOOK_HOST = "https://hr-job-bot.onrender.com"
+WEBHOOK_PATH = f"/webhook/{TOKEN}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get("PORT", 10000))
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
+
+if __name__ == "__main__":
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
