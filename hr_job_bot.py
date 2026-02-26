a8d2e572cd5749909b9f9993fd4276fad09e7c20
Elyorbek Xalmuxammatovich Rahmanov <132456769+RahmanovElyorbek@users.noreply.github.com>
2026-02-26 21:43:24 +0500

    remove shutdown webhook bug

--- hr_job_bot.py
+++ hr_job_bot.py
@@ -215,9 +215,7 @@ async def on_startup(dp):
     logging.warning("✅ Webhook ishga tushdi!")
 
 
-async def on_shutdown(dp):
-    await bot.delete_webhook()
-    logging.warning("⛔ Webhook o‘chirildi!")
+
 
 
 if __name__ == "__main__":
@@ -225,6 +223,6 @@ if __name__ == "__main__":
     app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_PATH)
 
     app.on_startup.append(on_startup)
-    app.on_shutdown.append(on_shutdown)
+    
 
     web.run_app(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
